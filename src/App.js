48e2a0a07a8439457ac0311c46fccfc8ac3e453f
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-10-23 20:50:43 +0100

    removing unecessary files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FirstQuestion from './FirstQuestion/FirstQuestion';
+import Questions from './Questions/Questions';
 
 function App() {
   return (
     <div className="App">
      <h1>Quiz App</h1>
-     <FirstQuestion/>
+     <Questions/>
     </div>
   );
 }
--- src/SecondQuestion/SecondQuestion.js
+++
@@ -1,8 +0,0 @@
-const { Typography } = require("@material-ui/core")
-import React from 'react';
-
-const SecondQuestion = () => {
-    return(
-        <Typography>Second Question</Typography>
-    )
-}
